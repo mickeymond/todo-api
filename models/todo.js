b335ceeaea438d0a670d2c5a08c50294308fe44a
Michael Hammond <33749130+mickeymond@users.noreply.github.com>
2024-10-15 23:34:19 +0000

    adding @reis/mongoose-to-json

--- models/todo.js
+++ models/todo.js
@@ -1,9 +1,12 @@
 import { Schema, model } from "mongoose";
+import { toJSON } from "@reis/mongoose-to-json";
 
 const todoSchema = new Schema({
     title: { type: String, required: true },
     icon: { type: String, required: true },
     completed: { type: Boolean, default: false }
 });
+
+todoSchema.plugin(toJSON);
 
 export const TodoModel = model('Todo', todoSchema);
